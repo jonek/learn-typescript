2c4d4f27b1ddd50dfc07c9550ce1d5cd9bf26b72
Johannes Eickhold <jeick@eclipsesource.com>
2017-02-08 16:43:49 +0100

    Add tslint support to project
    This includes a tslint task for visual studio code.
    Run task "tslint" to lint all files of the project in one go. Note: you have to stop the background build task to do so.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 var sourcemaps = require('gulp-sourcemaps');
+var tslint = require('gulp-tslint');
 
-gulp.task('compile', function () {
+gulp.task('compile', function() {
     return tsProject
         .src()
         .pipe(sourcemaps.init())
         .pipe(tsProject())
         .js
-        .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../src'}))
+        .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../src' }))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('tslint', () => {
+    return gulp.src(['**/*.ts', '!**/*.d.ts', '!node_modules/**'])
+        .pipe(tslint())
+        .pipe(tslint.report());
 });
 
 gulp.task('watch', ['compile'], function() {
